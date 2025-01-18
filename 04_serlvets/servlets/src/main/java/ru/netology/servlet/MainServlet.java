e891feb28ee566ce6a4c852c8b70c3a3b792fa89
Yunona <guzel.ru.2015@mail.ru>
2025-01-18 10:06:23 +0500

    Your commit message

---
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,53 @@
+package ru.netology.servlet;
+
+import ru.netology.config.AppConfig;
+import ru.netology.controller.PostController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+public class MainServlet extends HttpServlet {
+  private PostController controller;
+
+  @Override
+  public void init() {
+    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+    context.register(AppConfig.class);
+    context.refresh();
+
+    controller = context.getBean(PostController.class);
+  }
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) {
+    try {
+      final var path = req.getRequestURI();
+      final var method = req.getMethod();
+      if (method.equals("GET") && path.equals("/api/posts")) {
+        controller.all(resp);
+        return;
+      }
+      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+        controller.getById(id, resp);
+        return;
+      }
+      if (method.equals("POST") && path.equals("/api/posts")) {
+        controller.save(req.getReader(), resp);
+        return;
+      }
+      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+        controller.removeById(id, resp);
+        return;
+      }
+      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    } catch (Exception e) {
+      e.printStackTrace();
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+}
